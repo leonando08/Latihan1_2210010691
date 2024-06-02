9a4d4ae11191b913d6df017faee8df55bb7df7df
leonando08 <rezaleonar50@gmail.com>
2024-06-03 01:57:29 +0800

    membuat constructor dan setPenjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author User
- */
 public class Matematika {
+    //membuat variabel
+    private double bil1,bil2;
+    
+    //constructor
+    public Matematika(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    
+    //membuat method
+    public double setPenjumlahan() {
+        return bil1 + bil2;
+    }
     
 }
