dff274def09f29e2636b592e2491de08754eec01
leonando08 <rezaleonar50@gmail.com>
2024-06-03 02:04:35 +0800

    membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hitung;
 
-/**
- *
- * @author User
- */
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        Matematika nando = new Matematika(9,1);
+        
+        System.out.println("Hasil Penjumlahan: " + nando.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: " + nando.setPengurangan());
+        System.out.println("Hasil Perkalian: " + nando.setPerkalian());
+        System.out.println("Hasil Pembagian: " + nando.setPembagian());
+    }
     
 }
